2ab8d2dc92011ab690e2dc985fc57024a579112e
iperdiscount <iperdiscount@gmail.com>
2012-10-05 20:09:07 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cms::Application.config.secret_token = '584434072db6795f4140a3360e0bbfb706737dade7609df3ec363273a112d571d958e2b6d04793a58b87049411bea0b70ef5d56ea7333f2f3d03097848bc55a8'
